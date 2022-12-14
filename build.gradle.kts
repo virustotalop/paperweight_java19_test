0a7bb4ac2383164549fd8cd308ab32ebd1bf8797
virustotalop <virustotalop@gmail.com>
2022-12-13 18:53:01 -0800

    Test using java 19 for compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ version = "1.0.0-SNAPSHOT"
 
 java {
     // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-    toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 repositories {
@@ -21,15 +21,16 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
 
+tasks.withType<JavaCompile>().configureEach {
+    javaCompiler.set(javaToolchains.compilerFor {
+        languageVersion.set(JavaLanguageVersion.of(19))
+    })
+}
+
 tasks {
     // Configure reobfJar to run when invoking the build task
     assemble {
         dependsOn(reobfJar)
-    }
-
-    compileJava {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-        options.release.set(17)
     }
 
     javadoc {
